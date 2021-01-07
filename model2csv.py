857a2c3a2eebbbd39081bb50832826278bec578f
tmart234 <63220256+tmart234@users.noreply.github.com>
2021-01-07 13:53:14 -0700

    Add files via upload

--- model2csv.py
+++ model2csv.py
@@ -1,15 +1,20 @@
-## this script will....
+## this script will parse a model's used elements
 ##
-## <a:KeyValueOfguidanyType> to find the elements used in the model
-## within element find <i:type="c:string">value</b:Value> for custom names
-## get custom element properties selection from <b:SelectedIndex> and the above
+
 
 import xml.etree.ElementTree as ET
 import csv
-import fnmatch
 
 tree = ET.parse('sample2.xml')
 root = tree.getroot()
+
+# set up dictionaries
+# multiple elements, values are a list of element dicts of those types
+elements = dict.fromkeys(['flows','stencils','boundarys','interactors'])
+# singular element dict
+element = dict.fromkeys(['guid','ID','name','SourceGuid','TargetGuid','EleProperties'])
+# create custom element properties dict as this part will change
+# from <b:SelectedIndex> and the properties above
 
 with open('model.csv', 'w', newline='') as r:
     writer = csv.writer(r)
@@ -18,10 +23,23 @@ with open('model.csv', 'w', newline='') as r:
 
     for child in root.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}DrawingSurfaceList'):
         for ele in child.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}DrawingSurfaceModel'):
+            # get Boarders too? anything else?
             for ele2 in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Lines'):
+                # this level contains a model's elements
                 for ele3 in ele2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}KeyValueOfguidanyType'):
                     for ele4 in ele3.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Value'):
+                        for guid in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}Guid'):
+                            element['guid'] = guid.text
+                        for gen_type in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}GenericTypeId'):
+                            element['id'] = gen_type.text
+                        for source in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}SourceGuid'):
+                            element['SourceGuid'] = source.text
+                        for source in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}TargetGuid'):
+                            element['TargetGuid'] = source.text
                         for ele5 in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}Properties'):
-                            for ele6 in ele5.iter():
-                                print(ele6.tag)
-                                print(ele6.text)
+                            for ele6 in ele5.iter('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}anyType'):
+                                for ele7 in ele6.iter():
+                                   if 'StringDisplayAttribute' in ET.tostring(ele7, method='text'):
+                                       print(ele7.attrib)
+                                # <i:type="c:string">value</b:Value> for custom names
+                                # add element to elements

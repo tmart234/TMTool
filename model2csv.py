7805b3ca49d51f5a6487d9c8c209e04c8621d84f
tmart234 <63220256+tmart234@users.noreply.github.com>
2021-01-15 18:47:27 -0700

    model2csv edits
    added elements to csv output

--- model2csv.py
+++ model2csv.py
@@ -30,38 +30,48 @@ element = dict.fromkeys(['GenericTypeId','GUID','Name','SourceGuid','TargetGuid'
 # create custom element properties dict as this part will change
 # from <b:SelectedIndex> and the properties above
 
+def write_element(ele2):
+         # this level enumerates a model's elements
+    for ele3 in ele2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}KeyValueOfguidanyType'):
+                        # GUID also at this level
+        for ele4 in ele3.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Value'):
+                            # get GUID
+            for guid in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}Guid'):
+                element['GUID'] = guid.text
+            for gen_type in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}GenericTypeId'):
+                element['GenericTypeId'] = gen_type.text
+            for source in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}SourceGuid'):
+                element['SourceGuid'] = source.text
+            for source in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}TargetGuid'):
+                element['TargetGuid'] = source.text
+            for ele5 in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}Properties'):
+                for ele6 in ele5.iter('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}anyType'):
+                    for ele7 in ele6.iter():
+                     # find element's the custom name ex: "HTTPS Device In"
+                            if "c:string" in str(ele7.attrib) and (ele7.text and ele7.text != '0'):
+                                element['Name'] = ele7.text
+                                # write element to csv row
+                                writer.writerow([element['GenericTypeId'], element['GUID'], element['Name'], element['SourceGuid'], element['TargetGuid']])
+                                            # TODO: get custom element properties selection from <b:SelectedIndex> and the <values> above
+
+
 with open('model.csv', 'w', newline='') as r:
     writer = csv.writer(r)
     # write headders in csv file
-    writer.writerow(['GenericTypeId','GUID','Name','SourceGuid','TargetGuid'])
 
     for child in root.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}DrawingSurfaceList'):
         for ele in child.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}DrawingSurfaceModel'):
-            # TODO: get <Boarders> too? anything else?
-            for ele2 in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Lines'):
-                # this level enumerates a model's elements
-                for ele3 in ele2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}KeyValueOfguidanyType'):
-                    # GUID also at this level
-                    for ele4 in ele3.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Value'):
-                        # get GUID
-                        for guid in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}Guid'):
-                            element['GUID'] = guid.text
-                        for gen_type in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}GenericTypeId'):
-                            element['GenericTypeId'] = gen_type.text
-                        for source in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}SourceGuid'):
-                            element['SourceGuid'] = source.text
-                        for source in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}TargetGuid'):
-                            element['TargetGuid'] = source.text
-                        for ele5 in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}Properties'):
-                            for ele6 in ele5.iter('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}anyType'):
-                                for ele7 in ele6.iter():
-                                      # find element's the custom name ex: "HTTPS Device In"
-                                        if "c:string" in str(ele7.attrib) and (ele7.text and ele7.text != '0'):
-                                            element['Name'] = ele7.text
-                                            # write element to csv row
-                                            writer.writerow([element['GenericTypeId'], element['GUID'], element['Name'], element['SourceGuid'], element['TargetGuid']])
-
-                                # TODO: get custom element properties selection from <b:SelectedIndex> and the <values> above
+            for borders in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Borders'):
+                # write element headders
+                writer.writerow(['Elements'])
+                writer.writerow(['GenericTypeId','GUID','Name'])
+                write_element(borders)
+            for lines in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Lines'):
+                writer.writerow([''])
+                writer.writerow(['Flows'])
+                writer.writerow(['GenericTypeId','GUID','Name','SourceGuid','TargetGuid'])
+                write_element(lines)
+               
     
 # delete temp .xml file created
 if os.path.exists(folder_path):

f663a3c7c7f3bd83b3e610b26dc889a914dd5622
Tyler <63220256+tmart234@users.noreply.github.com>
2021-02-15 16:42:00 -0700

    pypi edits

---
+++ TMT/comand_line.py
@@ -0,0 +1,4 @@
+import argparse
+
+def main():
+    print('CLI entry')
--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@ import os
 
 thelibFolder = os.path.dirname(os.path.realpath(__file__))
 requirementPath = thelibFolder + '/requirements.txt'
-install_requires = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
 if os.path.isfile(requirementPath):
     with open(requirementPath) as f:
-        install_requires = f.read().splitlines()
+        requirements = f.read().splitlines()
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='TMTool',
-    version='0.0.5',
+    version='0.0.8',
     author_email="tmart234@gmail.com",
     description='python tool for Microsoft Threat modeling tool',
     url="https://github.com/tmart234/TMT",
@@ -25,5 +24,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
+    install_requires = requirements,
+    entry_points = {
+        'console_scripts': ['TMTool=TMTool.command_line:main'],
+    }
 )

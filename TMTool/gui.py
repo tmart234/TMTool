44f6e08ba987cf7e0a23a21784811662fb50111f
Tyler <63220256+tmart234@users.noreply.github.com>
2021-02-16 18:58:26 -0700

    dark mode

--- TMTool/gui.py
+++ TMTool/gui.py
@@ -1,5 +1,10 @@
-import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
+# override the basic Tk widgets
+from tkinter.ttk import *
+from tkinter import *
+from ttkthemes import ThemedStyle
+
 from TMTool.Scripts import template2xlsx
 from TMTool.Scripts import xlsx2template
 from TMTool.Scripts import jira_issues
@@ -48,44 +53,58 @@ def open_script5():
 
 
 def main():
-    root = tk.Tk()
+    root = Tk()
+    root.configure(background='#404040')
     root.title("TMTool GUI")
-    frame = tk.Frame(root)
-    frame.pack()
+    # Setting Theme
+    style = ThemedStyle(root)
+    style.set_theme("equilux")
+
+
+    # for the main area of the window
+    window = ttk.Frame(root)
+    window.pack(fill=X, side=TOP)
 
     # template scripts (green background)
-    button1 = tk.Button(frame, 
+    button1 = ttk.Button(window, 
                     text="template2xlsx", 
-                    command=open_script1,
-                    fg = "white",
-                    bg="green")
-    button1.pack(side=tk.LEFT)
-    button2 = tk.Button(frame,
+                    command=open_script1)
+
+    button2 = ttk.Button(window,
                     text="xlsx2template",
-                    command=open_script2,
-                    fg = "white",
-                    bg="green")
-    button2.pack(side=tk.LEFT)
+                    command=open_script2)
 
     # model scripts (blue)
-    button3 = tk.Button(frame,
+    button3 = ttk.Button(window,
                     text="upload jira issues",
-                    command=open_script3,
-                    fg = "white",
-                    bg="blue")
-    button3.pack(side=tk.RIGHT)
-    button4 = tk.Button(frame,
+                    command=open_script3)
+
+    button4 = ttk.Button(window,
                     text="fix report hyperlinks",
-                    command=open_script4,
-                    fg = "white",
-                    bg="blue")
-    button4.pack(side=tk.RIGHT)
-    button5 = tk.Button(frame,
+                    command=open_script4)
+
+    button5 = ttk.Button(window,
                     text="model2csv",
-                    command=open_script5,
-                    fg = "white",
-                    bg="blue")
-    button5.pack(side=tk.BOTTOM)
+                    command=open_script5)
+
+    # light green
+    text1 = ttk.Label(window,
+                    text="Template Scripts:", foreground='#CCFFCC')
+    # light blue
+    text2 = ttk.Label(window,
+                    text="Model Scripts:", foreground='#CCE5FF')
+
+    # configure columns
+    window.columnconfigure(0, weight=1)
+    window.columnconfigure(1, weight=1)
+    # make buttons grow when window and cells grow  
+    text1.grid(row=0, column=0, sticky=W+E)
+    text2.grid(row=0, column=1, sticky=W+E)
+    button1.grid(row=1, column=0, sticky=W+E)
+    button2.grid(row=2, column=0, sticky=W+E)
+    button3.grid(row=1, column=1, sticky=W+E)
+    button4.grid(row=2, column=1, sticky=W+E)
+    button5.grid(row=3, column=1, sticky=W+E)
     root.mainloop()
 
 

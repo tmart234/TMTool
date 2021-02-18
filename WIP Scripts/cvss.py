aae96cdd498dcb3ef367988ccb2d065a3d0d5577
Tyler <63220256+tmart234@users.noreply.github.com>
2021-02-17 21:53:01 -0700

    cvss scoring

--- WIP Scripts/cvss.py
+++ WIP Scripts/cvss.py
@@ -1,5 +1,4 @@
-# CVSS V2, Base scoring only
-# work in progress
+# CVSS V2 scoring
 # use https://nvd.nist.gov/vuln-metrics/cvss/v2-calculator to check yourself
 
 def get_ac(access_complexity):
@@ -76,44 +75,86 @@ def get_cdp(cdp):
         return 0.4
     elif cdp is 'H':
         return 0.5
-    return
+    else:
+        return 0
+
+def get_TD(td):
+    if td is 'N':
+        return 0
+    elif td is 'L':
+        return 0.25
+    elif td is 'M':
+        return 0.75
+    elif td is 'H':
+        return 1.0
+    else:
+        return 1.0
+
+def get_conf_req(req):
+    if req is 'L':
+        return 0.5
+    elif req is 'M':
+        return 1.0
+    elif req is 'H':
+        return 1.51
+    else:
+        return 1.0
+
+def get_integ_req(req):
+    if req is 'L':
+        return 0.5
+    elif req is 'M':
+        return 1.0
+    elif req is 'H':
+        return 1.51
+    else:
+        return 1.0
+
+def get_avail_req(req):
+    if req is 'L':
+        return 0.5
+    elif req is 'M':
+        return 1.0
+    elif req is 'H':
+        return 1.51
+    else:
+        return 1.0
+
 
 class CVSS():
-    def __init__(self):
+    # takes in a cvss dict
+    def __init__(self, cvss_dict):
         # static value, assume target exists in env on a considerable scale
         # TODO: input Security requirements and TD
         self.TD = 1.0
         self.temporal_score = None
-        self.base = dict.fromkeys('AV','AC','AU','C','I','A')
-        self.env = dict.fromkeys('CPD','TD','CR','IR','AR')
-        # TODO: figure out how were importing values here
-        # then calculate the base score
-        self.base_score = self.calulate_base()
+        self.cvss_dict = cvss_dict
+        self.calulate_base()
         # since temporal is not used
         self.temporal = self.base_score
-        self.env_score = self.calulate_env()
+        self.calulate_env()
         self.calulate_overall()
+        return
 
     # TODO: get values from dict and get_() functions above
     def calulate_base(self):
-        self.impact = 10.41 * (1 - (1 - self.conf_impact) * (1 - self.integ_impact) * (1 - self.aval_impact))
-        self.exploitability = 20 * self.access_complexity * self.authentication * self.access_vector
-        if self.impact == 0:
-            self.f_Impact = 0
-        else:
-            self.f_Impact = 1.176
-        self.base_score = (0.6*self.impact) + (((0.4*self.exploitability)-1.5)*self.f_impact)
+        self.impact = round(10.41 * (1 - (1 - get_conf(self.cvss_dict.get('C'))) * (1 - get_integ(self.cvss_dict.get('I'))) * (1 - get_avail(self.cvss_dict.get('A')))), 1)
+        self.exploitability = round(20 * get_ac(self.cvss_dict.get('AC')) * get_auth(self.cvss_dict.get('Au')) * get_av(self.cvss_dict.get('AV')),1)
+        self.f_impact = 0
+        if self.impact > 0:
+            self.f_impact = 1.176
+        self.base_score = round((((0.6*self.impact) + (0.4*self.exploitability)-1.5) * self.f_impact), 1)
         return
 
     def calulate_env(self):
-        self.env = ((self.adj_temp + (10 - self.adj_temp) * self.CDP) * self.TD)
+        _conf = (1 - get_conf(self.cvss_dict.get('C')) * get_conf_req(self.cvss_dict.get('CR')))
+        _integ = (1 - get_integ(self.cvss_dict.get('I')) * get_integ_req(self.cvss_dict.get('IR')))
+        _avail = (1 - get_avail(self.cvss_dict.get('A')) * get_avail_req(self.cvss_dict.get('AR')))
+        self.adj_impact = min(10, (10.41 * (1 - (_conf) * (_integ) * (_avail))))
+        self.adj_temp = round((((0.6*self.adj_impact) + (0.4*self.exploitability)-1.5) * self.f_impact), 1)
+        self.env_score = round(((self.adj_temp + (10 - self.adj_temp) * get_cdp(self.cvss_dict.get('CDP'))) * get_TD(self.cvss_dict.get('TD'))), 1)
         return
- 
-# self.adj_temp = TemporalScore recomputed with the Impact sub-equation 
-#                    replaced with the following self.adj_impact equation.
- 
-# self.adj_impact = min(10, (10.41 * (1 - (1 - self.conf_impact * ConfReq) * (1 - self.integ_impact * IntegReq) * (1 - self.aval_impact * AvailReq))))
- 
+
     # See overall score decision tree
     def calulate_overall(self):
         if self.env_score:
@@ -122,3 +163,20 @@ class CVSS():
             self.overall_score = self.temporal_score
         else:
             self.overall_score = self.base_score
+
+def main():
+    # CVSS Base Score: 5.4
+    # Impact Subscore: 6.9
+    # Exploitability Subscore: 4.9
+    # CVSS Temporal Score: NA
+    # CVSS Environmental Score: 8.6
+    # Modified Impact Subscore: 10.0
+    # Overall CVSS Score: 8.6
+    # bullshit example: 
+    # https://nvd.nist.gov/vuln-metrics/cvss/v2-calculator?vector=(AV:N/AC:H/Au:N/C:C/I:N/A:N/CDP:MH/TD:H/CR:H/IR:H/AR:L)
+    mydict =  {'AV':'N','AC':'H','Au':'N','C':'C','I':'N','A':'N','CDP':'MH','TD':'H','CR':'H','IR':'H','AR':'L'}
+    score= CVSS(mydict)
+    print(score.overall_score)
+
+if __name__ == "__main__":
+    main()

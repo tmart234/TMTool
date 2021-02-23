7f04e408f1c79867b9fedf2e44f326aec1965943
Tyler <63220256+tmart234@users.noreply.github.com>
2021-02-23 14:04:40 -0700

    scoring updates

--- WIP Scripts/model_score.py
+++ WIP Scripts/model_score.py
@@ -3,12 +3,12 @@
 ## load metadata (notes) and threat props into cvss dicts, and score dicts w/ cvss.py
 ## TODO: this method currently involves setting CIA + severity (CVSS threat props) in analysis mode (solve w/ data classification)
 
-
 from lxml import etree
 import tkinter as tk
 from tkinter import filedialog
+import json
 
-def get_element(ele2):
+def get_element(_ele):
     # set up dictionaries
     # single element dict
     element = dict.fromkeys(['GenericTypeId','GUID','Name','SourceGuid','TargetGuid', 'properties'])
@@ -20,60 +20,93 @@ def get_element(ele2):
     ele_props = []
     # temp list of property values
     _values = []
-
-         # this level enumerates a model's elements
-    for ele3 in ele2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}KeyValueOfguidanyType'):
-                        # GUID also at this level
-        for ele4 in ele3.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Value'):
-            # get GUID
-            for guid in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}Guid'):
-                element['GUID'] = guid.text
-            for gen_type in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}GenericTypeId'):
-                element['GenericTypeId'] = gen_type.text
-            for source in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}SourceGuid'):
-                element['SourceGuid'] = source.text
-            for target in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}TargetGuid'):
-                element['TargetGuid'] = target.text
-            for props in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}Properties'):
-                for types in props.findall('.//a:anyType', any_namespace):
-                # get all child elements of anyType element, all properties located here
-                    for dis_name in types.findall('.//b:DisplayName', ele_namespace):   
-                        ele_prop['PropName'] = dis_name.text
-                    for prop_guid in types.findall('.//b:Name', ele_namespace):   
-                        if prop_guid.text:
-                            ele_prop['PropGUID'] = prop_guid.text
-                        else:
-                            ele_prop['PropGUID'] = ''
-                    selection = types.find('.//b:SelectedIndex', ele_namespace)   
-                    if selection is None:
-                        ele_prop['SelectedIndex'] = ''
-                        # get all prop values
-                        value = types.find('.//b:Value', ele_namespace)
-                        # set values
-                        if value.text is None:
-                            _values.append('')
-                        else:
-                            _values.append(value.text)
-                        # set custom element name 
-                        if ele_prop['PropName'] == 'Name':
-                            element['Name'] = value.text
-                            ele_prop['PropValues'] = _values.copy()
+                    # GUID also at this level
+    for ele4 in _ele.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Value'):
+        # get GUID
+        for guid in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}Guid'):
+            element['GUID'] = guid.text
+        for gen_type in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}GenericTypeId'):
+            element['GenericTypeId'] = gen_type.text
+        for source in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}SourceGuid'):
+            element['SourceGuid'] = source.text
+        for target in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}TargetGuid'):
+            element['TargetGuid'] = target.text
+        for props in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}Properties'):
+            for types in props.findall('.//a:anyType', any_namespace):
+            # get all child elements of anyType element, all properties located here
+                for dis_name in types.findall('.//b:DisplayName', ele_namespace):   
+                    ele_prop['PropName'] = dis_name.text
+                for prop_guid in types.findall('.//b:Name', ele_namespace):   
+                    if prop_guid.text:
+                        ele_prop['PropGUID'] = prop_guid.text
                     else:
-                        # get prop selection
-                        ele_prop['SelectedIndex'] = selection.text
-                        # get value list for selection
-                        for values in types.findall('.//b:Value/*', ele_namespace):
-                            _values.append(values.text)
+                        ele_prop['PropGUID'] = ''
+                selection = types.find('.//b:SelectedIndex', ele_namespace)   
+                if selection is None:
+                    ele_prop['SelectedIndex'] = ''
+                    # get all prop values
+                    value = types.find('.//b:Value', ele_namespace)
+                    # set values
+                    if value.text is None:
+                        _values.append('')
+                    else:
+                        _values.append(value.text)
+                    # set custom element name 
+                    if ele_prop['PropName'] == 'Name':
+                        element['Name'] = value.text
                         ele_prop['PropValues'] = _values.copy()
-                     # add prop to prop list
-                    _values.clear()
-                    ele_props.append(ele_prop.copy())
-                    ele_prop.clear()
-            # save prop list to element dict
-            element['properties'] = ele_props
-            # print(element['properties'])
-            ele_props.clear()
-            return element['GUID'], element
+                else:
+                    # get prop selection
+                    ele_prop['SelectedIndex'] = selection.text
+                    # get value list for selection
+                    for values in types.findall('.//b:Value/*', ele_namespace):
+                        _values.append(values.text)
+                    ele_prop['PropValues'] = _values.copy()
+                    # add prop to prop list
+                _values.clear()
+                ele_props.append(ele_prop.copy())
+                ele_prop.clear()
+        # save prop list to element dict
+        element['properties'] = ele_props
+        # print(element['properties'])
+        ele_props.clear()
+        return element
+
+def get_meta(root):
+    # find all note elements
+    _id = ''
+    msgs = dict()
+    for notes in root.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Notes'):
+        for note in notes.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Note'):
+            for id in note.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Id'):
+                _id = id.text
+            for message in note.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Message'):
+                _message = message.text
+            msgs[_id] = _message
+    for key,value in msgs.items():
+        if "METADATA:" in value:
+            _meta = str(value).replace("METADATA:","").replace("\'", "\"")
+            return dict(json.loads(_meta))
+    return None
+
+
+def get_flows(root):
+    flows = dict()
+    for child in root.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}DrawingSurfaceList'):
+        for drawing in child.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}DrawingSurfaceModel'):
+            # dont need borders currently
+            # for borders in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Borders'):
+            #     _id, ele = get_element(borders)
+            #     elements[_id] = ele
+            for lines in drawing.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Lines'):
+                # this level enumerates a model's elements
+                for line in lines.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}KeyValueOfguidanyType'):
+                # unlike stencils, flows have a source and target guids
+                    ele = get_element(line)
+                    flows[ele.get('GUID')] = ele
+            return flows
+    return None
+
 
 def main():
     root = tk.Tk()
@@ -87,40 +120,17 @@ def main():
     if not file_path:
         print('Must choose file path, quitting... ')
         quit()
-    # get CSV file
-    try:
-        threat_path = filedialog.askopenfilename(parent=root, filetypes=[("threat csv file", "*.csv")])
-    except FileNotFoundError:
-        print('Must choose file path, quitting... ')
-        quit()
-    if not threat_path:
-        print('Must choose file path, quitting... ')
-        quit()
+
     root.destroy()
     tree = etree.parse(file_path)
     root = tree.getroot()
 
-    elements = dict()
-    for child in root.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}DrawingSurfaceList'):
-        for ele in child.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}DrawingSurfaceModel'):
-            for borders in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Borders'):
-                _id, ele = get_element(borders)
-                elements[_id] = ele
-            for lines in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Lines'):
-                # unlike stencils, flows have a source and target guids
-                _id, ele = get_element(lines)
-                elements[_id] = ele
+    meta = get_meta(root)
+    if not meta:
+        print('No meta found, choosing defualts')
 
-        # find all note elements
-        _id = ''
-        msgs = dict()
-        for notes in root.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Notes'):
-            for note in notes.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Note'):
-                for id in note.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Id'):
-                    _id = id.text
-                for message in note.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Message'):
-                    _message = message.text
-                msgs[_id] = _message
+    flows = get_flows(root)
+    print(flows)
 
 
 if __name__ == '__main__':

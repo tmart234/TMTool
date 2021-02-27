7ee179ceff820269bb2a3027077fef0db74e6731
Tyler <63220256+tmart234@users.noreply.github.com>
2021-02-27 11:47:24 -0700

    Update asset_form.py

--- WIP Scripts/asset_form.py
+++ WIP Scripts/asset_form.py
@@ -14,94 +14,108 @@ from tkinter import ttk
 from tkinter import *
 from ttkthemes import ThemedStyle
 
-def get_boxes_and_destroy():
-    quit()
+class AssetsWindow(ttk.Frame):
+    def __init__(self, master, flows, *args, **kwargs):
+        self.master = master
+        self.flows = flows
+        self.Assets = dict()
 
-def quit(_root):
-    _root.destroy()
+        self.master.configure(background='#404040')
+        self.master.title('ASSET FORM')
+        style = ThemedStyle(self.master)
+        style.set_theme("equilux")
+        ttk.Frame.__init__(self, master, *args, **kwargs)
+        self.label_frame = ttk.Frame(self.master)
+        self.label_frame.grid()
+        self.label_list = []
 
-def new_asset(_root, _flows):
-    new_f = tk.Tk()
-    new_f.configure(background='#404040')
-    new_f.title('NEW ASSET')
-    style = ThemedStyle(new_f)
-    style.set_theme("equilux")
+        self.label_list.append(ttk.Label(self.label_frame, text="List of Assets"))
+        self.label_list.append(ttk.Button(self.label_frame, text="Add new Asset", command=self.add_new_data))
+        for ndex, i in enumerate(self.label_list):
+            i.grid(row=ndex)
 
-    a_B1 = ttk.Button(new_f, text="Add", command=lambda: quit(new_f))
-    _lb1 = Listbox(new_f, selectmode='multiple', background='#404040', foreground='#FFFFFF')
-    _t1 = ttk.Label(new_f, text="Asset Severity:")
-    _t2 = ttk.Label(new_f, text="Flows Impacted:")
-    safe_data=("No Injuries", "Light Injuries", "Severe Injuries", "Life-threatening")
-    fin_data=("No Financial Impact", "Low Impact", "Substantial Loss", "Devastating")
-    op_data=("No Effect", "Minor Disruption", "Moderate Disruption", "Major Disruption", "Fails Saftey Reqs")
-    priv_data=("No Effect", "Low", "Medium", "High")
-    _t3 = ttk.Label(new_f, text="Asset Safty Impact")
-    _t4 = ttk.Label(new_f, text="Asset Financial Impact")
-    _t5 = ttk.Label(new_f, text="Asset Operational Impact")
-    _t6 = ttk.Label(new_f, text="Asset Privacy Impact")
-    _t7 = ttk.Label(new_f, text="Asset Name:")
-    _e1 = ttk.Entry(new_f)
-    _cb1 = ttk.Combobox(new_f, values=safe_data)
-    _cb2 = ttk.Combobox(new_f, values=fin_data)
-    _cb3 = ttk.Combobox(new_f, values=op_data)
-    _cb4 = ttk.Combobox(new_f, values=priv_data)
+    # open a user input form for adding an asset that will be shown on the root screen
+    def add_new_data(self):
+        self.new_f = tk.Tk()
+        self.new_f.configure(background='#404040')
+        self.new_f.title('NEW ASSET')
+        style = ThemedStyle(self.new_f)
+        style.set_theme("equilux")
 
-    # get list of model flow names
-    flo_names = list()
-    for i in range(0, len(_flows)):
-        flo_names.append(str(dict(list(_flows.values())[i]).get('Name')))
-    # fill Listbox    
-    for item in flo_names:
-        _lb1.insert('end', item)
-    
-    new_f.rowconfigure(0, weight=1)
-    new_f.rowconfigure(1, weight=1)
-    new_f.columnconfigure(0, weight=1)
-    new_f.columnconfigure(1, weight=1)
+        a_B1 = ttk.Button(self.new_f, text="Add", command=self.quit_new_asset)
+        _lb1 = Listbox(self.new_f, selectmode='multiple', background='#404040', foreground='#FFFFFF')
+        _t1 = ttk.Label(self.new_f, text="Asset Severity:")
+        _t2 = ttk.Label(self.new_f, text="Flows Impacted:")
+        safe_data=("No Injuries", "Light Injuries", "Severe Injuries", "Life-threatening")
+        fin_data=("No Financial Impact", "Low Impact", "Substantial Loss", "Devastating")
+        op_data=("No Effect", "Minor Disruption", "Moderate Disruption", "Major Disruption", "Fails Saftey Reqs")
+        priv_data=("No Effect", "Low", "Medium", "High")
+        _t3 = ttk.Label(self.new_f, text="Asset Safty Impact")
+        _t4 = ttk.Label(self.new_f, text="Asset Financial Impact")
+        _t5 = ttk.Label(self.new_f, text="Asset Operational Impact")
+        _t6 = ttk.Label(self.new_f, text="Asset Privacy Impact")
+        _t7 = ttk.Label(self.new_f, text="Asset Name:")
+        _e1 = ttk.Entry(self.new_f)
+        _cb1 = ttk.Combobox(self.new_f, values=safe_data)
+        _cb2 = ttk.Combobox(self.new_f, values=fin_data)
+        _cb3 = ttk.Combobox(self.new_f, values=op_data)
+        _cb4 = ttk.Combobox(self.new_f, values=priv_data)
 
-    _t7.grid(row=0, column= 0, sticky='nsew')
-    _e1.grid(row=1, column= 0, sticky='nsew')
-    _t2.grid(row=2, column= 0, sticky='nsew')
-    _lb1.grid(row=3, rowspan=6, column= 0, sticky='nsew')
-    _t1.grid(row=0, column= 1, sticky='nsew')
-    _t3.grid(row=1, column= 1, sticky='nsew')
-    _cb1.grid(row=2, column= 1, sticky='nsew')
-    _t4.grid(row=3, column= 1, sticky='nsew')
-    _cb2.grid(row=4, column= 1, sticky='nsew')
-    _t5.grid(row=5, column= 1, sticky='nsew')
-    _cb3.grid(row=6, column= 1, sticky='nsew')
-    _t6.grid(row=7, column= 1, sticky='nsew')
-    _cb4.grid(row=8, column= 1, sticky='nsew')
-    a_B1.grid(row=9, column= 0, columnspan=2, sticky='nsew')
-    new_f.mainloop()
-    # reset root geometry
-    if len(Assets) is None:
-        _len = 1
-    else:
-        _len = len(Assets)+1
-    # add to Assets
-    Assets[_len+1] = 'new'
-    x = 200
-    y = (_len*35)
-    _root.geometry(str(x) + "x" + str(y))
-    new_f.destroy()
-    return
+        # get list of model flow names
+        flo_names = list()
+        for i in range(0, len(self.flows)):
+            flo_names.append(str(dict(list(self.flows.values())[i]).get('Name')))
+        # fill Listbox    
+        for item in flo_names:
+            _lb1.insert('end', item)
+        
+        self.new_f.rowconfigure(0, weight=1)
+        self.new_f.rowconfigure(1, weight=1)
+        self.new_f.columnconfigure(0, weight=1)
+        self.new_f.columnconfigure(1, weight=1)
+
+        _t7.grid(row=0, column= 0, sticky='nsew')
+        _e1.grid(row=1, column= 0, sticky='nsew')
+        _t2.grid(row=2, column= 0, sticky='nsew')
+        _lb1.grid(row=3, rowspan=6, column= 0, sticky='nsew')
+        _t1.grid(row=0, column= 1, sticky='nsew')
+        _t3.grid(row=1, column= 1, sticky='nsew')
+        _cb1.grid(row=2, column= 1, sticky='nsew')
+        _t4.grid(row=3, column= 1, sticky='nsew')
+        _cb2.grid(row=4, column= 1, sticky='nsew')
+        _t5.grid(row=5, column= 1, sticky='nsew')
+        _cb3.grid(row=6, column= 1, sticky='nsew')
+        _t6.grid(row=7, column= 1, sticky='nsew')
+        _cb4.grid(row=8, column= 1, sticky='nsew')
+        a_B1.grid(row=9, column= 0, columnspan=2, sticky='nsew')
+        self.new_f.mainloop()
+
+    ## quit the "new asset" window
+    def quit_new_asset(self):
+        if not len(self.Assets):
+            a_len = 1
+        else:
+            a_len = len(self.Assets)+1
+        # add asset to dict
+        self.Assets[a_len]='new'+str(a_len)
+        # add label
+        self.label_list.insert(0, ttk.Label(self.label_frame, text=self.Assets.get(a_len)))
+        for widget in self.label_frame.children.values():
+            widget.grid_forget() 
+        for ndex, i in enumerate(self.label_list):
+            i.grid(row=ndex)
+        # resize root
+        x = 200
+        y = (a_len*35+25)
+        self.master.geometry(str(x) + "x" + str(y))
+
+        self.new_f.destroy()
 
 
 def main(flows):
-    global Assets
-    Assets = dict()
     root = tk.Tk()
-    root.configure(background='#404040')
-    root.title('ASSET FORM')
-    style = ThemedStyle(root)
-    style.set_theme("equilux")
-    
-    root.rowconfigure(0, weight=1)
-
-    B1 = ttk.Button(text="Add Asset", command=lambda: new_asset(root, flows))
-    B1.grid(row=0, column= 0, sticky='nsew')
-    root.mainloop()
+    app = AssetsWindow(root, flows)
+    app.mainloop()
 
 if __name__ == '__main__':
    main()
